0fb3c2f5f9d7decf18643dc9a0674e3a4f48a65b
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2021-03-25 16:03:28 +0100

    +Transfrom animation to channel

--- APP/Assets/index.js
+++ APP/Assets/index.js
@@ -3,7 +3,63 @@ let ChannelBox = document.getElementById('chanl');
 let UsernameBox = document.getElementById('user');
 let AlertBox = document.getElementById('Alert-box');
 let AlertBoxText = document.getElementById('Alert-box-text');
+let Main = document.getElementById('main');
+let MainForm = document.getElementById('main-form');
+let ContinueBtn = document.getElementById('continue-btn');
+let SendButton = document.getElementById("SendButton");
 let IndexLoad = true;
+let Connected = false;
+let InChat = false;
+
+async function PageTransform() {
+    if (InChat == false) {
+        ChannelBox.id = "MsgEdit";
+        ChannelBox = document.getElementById("MsgEdit");
+        ContinueBtn.style.transition = "0.3s";
+        ContinueBtn.style.display = "none";
+        ContinueBtn.style.opacity = "0";
+        ContinueBtn.disabled = true;
+    
+        UsernameBox.style.transition = "0.3s";
+        UsernameBox.style.opacity = "0";
+        UsernameBox.style.display = "none";
+        UsernameBox.style.margin = "0px";
+    
+        ChannelBox.style.transition = "0.3s";
+        ChannelBox.placeholder = "";
+        ChannelBox.style.textAlign = "left";
+        ChannelBox.style.border = "none";
+        ChannelBox.style.margin = "0px";
+        ChannelBox.style.height = "40px";
+        setTimeout(() => {
+            Main.style.transition = "0.3s";
+            MainForm.style.margin = "4px 10px 5px 10px";
+            Main.style.height = "60px";
+        }, 400);
+        setTimeout(() => {
+            MainForm.style.flexDirection = 'row';
+            MainForm.style.maxWidth = "none";
+            Main.style.top = "93%";
+            SendButton.style.display = "block";
+            SendButton.style.marginLeft = "-40px";
+        }, 800);
+        ChannelBox.addEventListener('focusin',() =>{
+            Main.style.transition = "0.5s";
+            MainForm.style.transition = "0.4s";
+            Main.style.maxWidth = "99%";
+            ChannelBox.style.flexGrow = "2";
+        });
+        ChannelBox.addEventListener('focusout',() =>{
+            Main.style.maxWidth = "420px";
+        });
+        InChat = true;
+    } else {
+        
+    }
+}
+
+
+
 
 async function AlertPop(content,poptime = 3000) {
     AlertBox.style.opacity = '1';
@@ -30,17 +86,21 @@ function ResponseHandler(ServerResponse) {
     }
 }
 
-async function SendData() {
-    event.preventDefault();
-    const response = await fetch('/Join', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            Username: UsernameBox.value,
-            ChannelID: ChannelBox.value
-        })
-    });
-    ResponseHandler(await response.json())
+async function SendFormData() {
+    if (InChat == false) {
+        event.preventDefault();
+        const response = await fetch('/Join', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                Username: UsernameBox.value,
+                ChannelID: ChannelBox.value
+            })
+        });
+        ResponseHandler(await response.json())
+    } else {
+        AlertPop("You are already in channel!", 4000);
+    }
 }
